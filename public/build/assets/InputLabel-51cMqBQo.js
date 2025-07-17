7054bf07daab72cd4a6f0f116f993ca6fab0bed4
RidhoIR <ridhoikhsandria348@gmail.com>
2025-07-17 16:59:49 +0700

    build

---
+++ public/build/assets/InputLabel-51cMqBQo.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-BD9pc2qE.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};
---
+++ public/build/assets/logo stai-DC2AgwFL.js
@@ -0,0 +1,1 @@
+const o="/build/assets/logo%20stai-Df5tLdkM.png";export{o as l};
